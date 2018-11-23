576e3571e1515eceeba7381235031b14e2346a59
Mukesh Kr singh <mukesh.puppy@gmail.com>
2018-11-23 16:43:55 +0530

    Add files via upload

--- logout.php
+++ logout.php
@@ -3,17 +3,23 @@ ob_start();
 session_start();
 require 'dbconnect.php';
 
+echo $_SESSION['user'] . '<br>';
+
+	$id = $_SESSION['user'];
+	echo $sql1="UPDATE `users` SET `status` = 'inactive' WHERE `users`.`id` = $id";
+	
 if(isset($_SESSION['user']) && $_SESSION['user'] != "") {
+	echo '<br>This inside';
 	$id = $_SESSION['user'];
 	$sql1="UPDATE `users` SET `status` = 'inactive' WHERE `users`.`id` = $id";
-	$res1=mysqli_query($conn, $sql1);
+	//$res1=mysqli_query($conn, $sql1);
 }
 else
 {
-	header("Location: login.php");
+	// header("Location: login.php");
 }
 
-session_destroy();
+/* session_destroy();
 unset($_SESSION['user']);
 unset($_SESSION['user_type']);
-exit;
+exit; */
